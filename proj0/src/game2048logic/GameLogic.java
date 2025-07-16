9b636576d87b11526df26b80fd3eadd14254ab9e
joye huang <huangdeshiou@gmail.com>
2025-07-16 21:22:50 +1000

    proj01: finsihed all tasks and passed all tests

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -2,8 +2,7 @@ package game2048logic;
 
 import game2048rendering.Board;
 import game2048rendering.Side;
-import static game2048logic.MatrixUtils.rotateLeft;
-import static game2048logic.MatrixUtils.rotateRight;
+import game2048logic.MatrixUtils;
 
 /**
  * @author  Josh Hug
@@ -20,18 +19,18 @@ public class GameLogic {
      *              if no merge occurs, then return 0.
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
-        if (r == 0){
+        if (r == 0) {
             return 0;
         }
-        for (int i = r; i > 0; i--){
+        for (int i = r; i > 0; i--) {
             /* move up because top is empty square*/
-            if (board[i-1][c] == 0 && i > minR ){
-                board[i-1][c] = board[i][c];
+            if (board[i - 1][c] == 0 && i > minR) {
+                board[i - 1][c] = board[i][c];
                 board[i][c] = 0;
             }
             /* merge because top has the same tile*/
-            else if (board[i-1][c] == board[i][c] && i > minR){
-                board[i-1][c] *= 2;
+            else if (board[i -1 ][c] == board[i][c] && i > minR) {
+                board[i - 1][c] *= 2;
                 board[i][c] = 0;
                 return i;
             }
@@ -51,10 +50,10 @@ public class GameLogic {
      */
     public static void tiltColumn(int[][] board, int c) {
         int mergedRow = -1;
-        for (int i = 1; i < board.length; i++){
-            if (board[i][c] != 0){
+        for (int i = 1; i < board.length; i++) {
+            if (board[i][c] != 0) {
                 int newMergedRow = moveTileUpAsFarAsPossible(board, i, c, mergedRow);
-                if (newMergedRow > mergedRow){
+                if (newMergedRow > mergedRow) {
                     mergedRow = newMergedRow;
                 }
             }
@@ -68,7 +67,7 @@ public class GameLogic {
      * @param board     the current state of the board.
      */
     public static void tiltUp(int[][] board) {
-        for (int i = 0; i < board.length; i++){
+        for (int i = 0; i < board.length; i++) {
             tiltColumn(board, i);
         }
         return;
@@ -82,14 +81,29 @@ public class GameLogic {
      * @param side  the direction to tilt
      */
     public static void tilt(int[][] board, Side side) {
-        // TODO: fill this in in task 7
+        int[] firstRow = board[0];
+        int[] secondRow = board[1];
+        int[] thirdRow = board[2];
+        int[] fourthRow = board[3];
         if (side == Side.EAST) {
+            MatrixUtils.rotateLeft(board);
+            tiltUp(board);
+            MatrixUtils.rotateRight(board);
             return;
         } else if (side == Side.WEST) {
+            MatrixUtils.rotateRight(board);
+            tiltUp(board);
+            MatrixUtils.rotateLeft(board);
             return;
         } else if (side == Side.SOUTH) {
+            MatrixUtils.rotateLeft(board);
+            MatrixUtils.rotateLeft(board);
+            tiltUp(board);
+            MatrixUtils.rotateRight(board);
+            MatrixUtils.rotateRight(board);
             return;
         } else {
+            tiltUp(board);
             return;
         }
     }
