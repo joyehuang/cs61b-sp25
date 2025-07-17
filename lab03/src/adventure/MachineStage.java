c178dc3f78efb8ef9d8c061b16534ad3d221999d
joye huang <huangdeshiou@gmail.com>
2025-07-17 19:43:08 +1000

    lab03: fixed MachineStage

--- lab03/src/adventure/MachineStage.java
+++ lab03/src/adventure/MachineStage.java
@@ -118,7 +118,7 @@ public class MachineStage implements AdventureStage {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = mysteryMax(a[i], b[i]);
+            int biggerValue = Integer.max(a[i], b[i]);
             returnArray[i] = biggerValue;
         }
 
@@ -132,7 +132,7 @@ public class MachineStage implements AdventureStage {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + mysteryAdd(sum, x[i]);
+            sum = sum + x[i];
             i = i + 1;
         }
         return sum;
