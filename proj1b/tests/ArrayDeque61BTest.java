a87fc0e7e03b0ce69c4a9155ee9bcf283fd99a90
joye huang <huangdeshiou@gmail.com>
2025-08-06 11:37:08 +1000

    proj1b: implement constructor and addFirst/addLast test

--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,10 +1,11 @@
 import deque.ArrayDeque61B;
 
 import jh61b.utils.Reflection;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.Deque;
 import java.util.List;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -22,4 +23,43 @@ public class ArrayDeque61BTest {
 //         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
 //     }
 
+    @Test
+    /** Basic test for addFirst method */
+    public void addFirstTestBasic(){
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+
+        // check if initial deque is null
+        assertThat(deque.toList()).isEmpty();
+
+        // adding element using addFirst()
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+
+        // check deque element and sequence after addFirst()
+        assertThat(deque.toList()).containsExactly(3, 2, 1);
+
+        // check size after adding element
+        assertThat(deque.size()).isEqualTo(3);
+    }
+
+    @Test
+    /** Basic test for addLast method */
+    public void addLastTestBasic(){
+        ArrayDeque61B<Integer> deque = new ArrayDeque61B<>();
+
+        // check if initial deque is null
+        assertThat(deque.toList()).isEmpty();
+
+        // adding element using addLast()
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+
+        // check deque element and sequence after addLast()
+        assertThat(deque.toList()).containsExactly(1, 2, 3);
+
+        // check size after adding element
+        assertThat(deque.size()).isEqualTo(3);
+    }
 }
